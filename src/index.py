77dd68225658bf8225f66e319b5c9baa69df47ff
Topias Harjunpää <topias.harjunpaa@gmail.com>
2021-03-22 23:28:13 +0200

    Fix

--- src/index.py
+++ src/index.py
@@ -3,8 +3,6 @@ from services.game_service import GameService
 from services.level_service import LevelService
 from services.render_service import RenderService
 from services.clock_service import ClockService
-from repositories.user_repository import user_repository
-from repositories.result_repository import result_repository
 
 WIDTH = 640
 HEIGHT = 480
